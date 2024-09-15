4002dc0174a00dcf9daaee57e21c6f3223def78f
Niji <47496813+Niji-Bemani@users.noreply.github.com>
2024-09-15 10:22:06 +0200

    Fix observed node and remove jQuery

--- smallYouTubeThumbnails.js
+++ smallYouTubeThumbnails.js
@@ -1,51 +1,42 @@
 // ==UserScript==
 // @name         Small YouTube Thumbnails
 // @namespace    http://tampermonkey.net/
-// @version      2024-09-14
+// @version      2024-09-15
 // @description  Reduce the size of YouTube thumbnails
 // @author       Niji Bemani
 // @match        https://www.youtube.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=youtube.com
 // @grant        none
-// @require      https://code.jquery.com/jquery-3.7.1.slim.min.js
+// @licence      MIT
 // ==/UserScript==
 
 (function() {
     'use strict';
 
-    // Clean elements
-    const clean = function() {
-        $('ytd-thumbnail').css('max-width', 0);
-        $('ytd-playlist-renderer').css('max-width', '50%');
-        $('ytd-radio-renderer').css('max-width', '50%');
-        //$('ytd-playlist-renderer').hide();
-        //$('ytd-radio-renderer').hide();
+    // Minimize all thumbnails
+    const minimizeThumbnails = function() {
+        document.querySelectorAll('ytd-thumbnail, ytd-playlist-thumbnail, ytd-channel-renderer #avatar-section')
+            .forEach(node => {node.style.maxWidth = 0});
     };
 
     // Add a MutationObserver on #contents element
-    const initObserver = function() {
+    const startObserver = function() {
         // Select the node that will be observed for mutations
-        const targetNode = document.getElementById("contents");
+        const targetNode = document.querySelector("ytd-app");
 
         // Callback function to execute when mutations are observed
         const callback = (mutationList, observer) => {
-            // Execute a clean if any of below elements are found in added nodes
-            const targetElements = ["YTD-THUMBNAIL", "YTD-PLAYLIST", "YTD-RADIO"];
+            // Minimize thumbnails if any of below elements are found in added nodes
+            const targetElements = ["YTD-VIDEO", "YTD-PLAYLIST", "YTD-CHANNEL", "YTD-RADIO"];
 
             // Loop through added nodes
-            let shouldClean = false;
             for (const mutation of mutationList) {
                 if (mutation.type === "childList" && mutation.addedNodes.length) {
-                    [].every.call(mutation.addedNodes, function(elm){
-                        if (targetElements.some(name => elm.nodeName.includes(name))) {
-                            shouldClean = true;
-                            return false;
+                    for (let index = 0; index < mutation.addedNodes.length; index++) {
+                        if (targetElements.some(targetName => mutation.addedNodes.item(index).nodeName.includes(targetName))) {
+                            return minimizeThumbnails();
                         }
-                    });
-                }
-                if (shouldClean) {
-                    clean();
-                    break;
+                    }
                 }
             }
         };
@@ -53,17 +44,14 @@
         // Create an observer instance linked to the callback function
         const observer = new MutationObserver(callback);
 
-        // Start observing
+        // Set observer to target node
         observer.observe(targetNode,{
             attributes: true, childList: true, subtree: true
         });
     };
 
-    // Clean at the begining, then for future elements
-    $(document).ready(function() {
-        setTimeout(function(){
-            clean();
-            initObserver();
-        }, 100);
-    });
+    // Start observer
+    setTimeout(function(){
+        startObserver();
+    }, 100);
 })();
